6b4942995719fbdc6170318fcd8935561ce355c2
priyapriyasomavarapu <priyapriyasomavarapu@gmail.com>
2026-06-15 20:29:54 +0530

    Controlstmt.java

---
+++ Controlstmt.java
@@ -0,0 +1,27 @@
+Control statements
+ When a program runs, statements execute one after another. But in real life, decisions and repetitions are needed.
+Control statements help us control the flow of execution.
+types:
+if,if else,elseif,nested ifelse, switch (conditional statements)
+for,while,do while (looping)
+break, continue,return( jump)
+
+String is a sequence of characters.
+Java
+String str = "Java";
+Important String Methods
+Method
+Example
+length() str.length()
+charAt() str.charAt(0)
+toUpperCase() Converts to uppercase
+toLowerCase() Converts to lowercase
+substring() Extracts part of string
+contains() Checks presence
+replace() Replaces characters
+trim() Removes spaces
+
+String s = "Java";
+
+System.out.println(s.length());
+System.out.println(s.toUpperCase());
