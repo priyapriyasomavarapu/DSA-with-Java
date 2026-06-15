76866cc631480b5686d34489d48e9b7db55fef47
priyapriyasomavarapu <priyapriyasomavarapu@gmail.com>
2026-06-15 20:07:35 +0530

    Basic.java

---
+++ Basic.java
@@ -0,0 +1,43 @@
+ Basics of Java
+-Java is a High level Language and it is a object oriented programming language 
+-It is also an platform-Independent language 
+
+Important terms in Java
+1.JDK---Java development kit ( development tools)
+Development+run
+2.JRE---Java Run-time Environment (libraries and files)
+run the code
+3.JVM---Java Virtual Machine (executive of bytecode)
+execution of the code 
+
+Input reading possibility in Java 
+1.by using scanner class 
+2.by using buffered reader class
+3.by using command line arguments 
+
+Java Program Structure
+class → Blueprint for objects.
+main() → Entry point of the program.
+System.out.println() → Prints output to the console.
+
+Operators in java
+1.Arthimetic 
+2.Relational
+3.Logical
+4.Bitwise
+5.Assignment
+6.Increment or Decrement 
+7.Conditional 
+
+
+Four Pillars of OOP
+Encapsulation – Wrapping data and methods together.
+Inheritance – Acquiring properties from another class.
+Polymorphism – One interface, many forms.
+Abstraction – Hiding implementation details.
+
+Common Collections:
+ArrayList
+LinkedList
+HashSet
+HashMap
