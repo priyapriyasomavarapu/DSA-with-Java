569117dceca708bb99ecf4268a66e7ce78d96a60
priyapriyasomavarapu <priyapriyasomavarapu@gmail.com>
2026-06-19 06:26:00 +0000

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,44 @@
+/*string counting
+import java.util.Scanner;
+class Day5
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        int count=0;
+        for(int i=0;i<str.length();i++)
+        {
+               count++;
+        }
+        System.out.println("String length is:"+str.length());
+
+
+    }
+}*/
+/*slicing--positive,negative*/
+/*import java.util.*;
+class Day5
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str="My class room of the cse and cseit students are very good and innocent people and talent person's";
+        String str1=str.substring(0,12);
+        System.out.println("String is:"+str1);
+    }
+}*/
+/*import java.util.*;
+class Day5
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str="My class room of the cse and cseit students are very good and innocent people and talent person's";
+        String str1=str.substring(str.length()-12);
+        System.out.println("String is:"+str1);
+    }
+}*
+/*reverse ofa string*/
+
+
